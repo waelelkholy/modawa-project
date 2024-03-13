bd3d17e0e568ed5759af06696f10772ede399b64
awaisali-sf <a.ismail@solutionfounder.com>
2024-03-13 15:12:28 +0500

    [IMP]

--- so_link_mo/models/mrp.py
+++ so_link_mo/models/mrp.py
@@ -1,7 +1,17 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import AccessError, UserError, ValidationError
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     recipe_id = fields.Many2one('sale.line.recipe')
+
+    def action_confirm(self):
+        res = super(MrpProduction, self).action_confirm()
+        if self.recipe_id:
+            if self.recipe_id.state != 'approve':
+                raise ValidationError(_(
+                    "You can't confirm because you recipe not approved yet"
+                ))
+        return res
